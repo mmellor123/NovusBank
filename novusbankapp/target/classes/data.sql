29f11d966fa0d13396c597abbfa6198566e1c1a4
mmellor123 <80254467+mmellor123@users.noreply.github.com>
2021-04-29 22:57:28 +0100

    Add files via upload

---
+++ novusbankapp/target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (account_number, first_name, last_name, email, password, balance) VALUES
+  (100, 'user1', 'usr1', 'user1@gmail.com', '$2a$10$iTj4iFK1M23uyigLbCkhRO3nWbGsxfD3KIiMr9OtbMJ8YpDX2o42m', 500),
+  (101, 'john', 'smith', 'j@gmail.com', '$2a$10$iTj4iFK1M23uyigLbCkhRO3nWbGsxfD3KIiMr9OtbMJ8YpDX2o42m', 200);
